46c4b2aec68608630b84ff9f81997f5419559d23
Will Hall <willhall88@hotmail.com>
2014-04-03 13:49:03 +0100

    made changes to gender checker function

--- alphalist.rb
+++ alphalist.rb
@@ -1,24 +1,24 @@
 
 students = [
 	{:name => "Steve Musgrave", :cohort => "March", :gender => "male"},
-	{:name => "Sroop Sunar", :cohort => "March", :gender => "male"},
+	{:name => "Sroop Sunar", :cohort => "March", :gender => "female"},
 	{:name => "Colin Marshall", :cohort => "March", :gender => "male"},
 	{:name => "Josh Fail-Brown", :cohort => "March", :gender => "male"},
-	{:name => "Louise Lai", :cohort => "March", :gender => "male"},
+	{:name => "Louise Lai", :cohort => "March", :gender => "female"},
 	{:name => "Robin Doble", :cohort => "March", :gender => "male"},
 	{:name => "Alex Wong", :cohort => "March", :gender => "male"},
 	{:name => "Scott Dimmock", :cohort => "March", :gender => "male"},
 	{:name => "Muhanad Al-Rubaiee", :cohort => "March", :gender => "male"},
-	{:name => "Shelley Hope", :cohort => "March", :gender => "male"},
+	{:name => "Shelley Hope", :cohort => "March", :gender => "female"},
 	{:name => "Will Hall", :cohort => "March", :gender => "male"},
 	{:name => "Oliver Delevingne", :cohort => "March", :gender => "male"},
 	{:name => "Nico Saueressig", :cohort => "March", :gender => "male"},
 	{:name => "Apostolis", :cohort => "March", :gender => "male"},
-	{:name => "Stefania", :cohort => "March", :gender => "male"},
+	{:name => "Stefania", :cohort => "March", :gender => "female"},
 	{:name => "Robert Leon", :cohort => "March", :gender => "male"},
-	{:name => "Emma Williams", :cohort => "March", :gender => "male"},
+	{:name => "Emma Williams", :cohort => "March", :gender => "female"},
 	{:name => "Joey Wolf", :cohort => "March", :gender => "male"},
-	{:name => "Julie Walker", :cohort => "March", :gender => "male"},
+	{:name => "Julie Walker", :cohort => "March", :gender => "female"},
 	{:name => "Daniel Le Dosquet", :cohort => "March", :gender => "male"},
 	{:name => "Mario Gintili", :cohort => "February", :gender => "male"},
 	{:name => "Mikhail Dubov", :cohort => "February", :gender => "male"},
@@ -75,13 +75,12 @@ def print_footer(students)
 		student[:gender].include? ("female")
 	end
 
-	boychecker = students.select do |student|
-		student[:gender].include? ("male")
-	end
+	boychecker = students.length - girlchecker.length
+
 	puts"The February cohort has #{febchecker.length} students."
 	puts"The March cohort has #{marchecker.length} students."
 	puts"The there are #{girlchecker.length} girls"
-	puts"The there are #{boychecker.length} boys!"
+	puts"The there are #{boychecker} boys!"
 	puts"All in all, there are #{students.length} makers!"
 end
 
