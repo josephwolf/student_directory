3b6de9b59c91b782e3f6fe386735d621f1614afd
Joey Wolf <halfdark000@gmail.com>
2014-04-03 13:04:04 -0700

    lots of new code! still broken

--- alphalist.rb
+++ alphalist.rb
@@ -1,55 +1,49 @@
 
-marchcohort = [
-	"Steve Musgrave",
-	"Sroop Sunar",
-	"Colin Marshall",
-	"Josh Fail-Brown",
-	"Louise Lai",
-	"Robin Doble",
-	"Alex Wong",
-	"Scott Dimmock",
-	"Muhanad Al-Rubaiee",
-	"Shelley Hope",
-	"Will Hall",
-	"Oliver Delevingne",
-	"Nico Saueressig",
-	"Apostolis",
-	"Stefania",
-	"Robert Leon",
-	"Emma Williams",
-	"Joey Wolf",
-	"Julie Walker",
-	"Daniel Le Dosquet"
+students = [
+	{:name => "Steve Musgrave", :cohort => "March", :gender => "male"},
+	{:name => "Sroop Sunar", :cohort => "March", :gender => "male"},
+	{:name => "Colin Marshall", :cohort => "March", :gender => "male"},
+	{:name => "Josh Fail-Brown", :cohort => "March", :gender => "male"},
+	{:name => "Louise Lai", :cohort => "March", :gender => "male"},
+	{:name => "Robin Doble", :cohort => "March", :gender => "male"},
+	{:name => "Alex Wong", :cohort => "March", :gender => "male"},
+	{:name => "Scott Dimmock", :cohort => "March", :gender => "male"},
+	{:name => "Muhanad Al-Rubaiee", :cohort => "March", :gender => "male"},
+	{:name => "Shelley Hope", :cohort => "March", :gender => "male"},
+	{:name => "Will Hall", :cohort => "March", :gender => "male"},
+	{:name => "Oliver Delevingne", :cohort => "March", :gender => "male"},
+	{:name => "Nico Saueressig", :cohort => "March", :gender => "male"},
+	{:name => "Apostolis", :cohort => "March", :gender => "male"},
+	{:name => "Stefania", :cohort => "March", :gender => "male"},
+	{:name => "Robert Leon", :cohort => "March", :gender => "male"},
+	{:name => "Emma Williams", :cohort => "March", :gender => "male"},
+	{:name => "Joey Wolf", :cohort => "March", :gender => "male"},
+	{:name => "Julie Walker", :cohort => "March", :gender => "male"},
+	{:name => "Daniel Le Dosquet", :cohort => "March", :gender => "male"},
+	{:name => "Mario Gintili", :cohort => "February", :gender => "male"},
+	{:name => "Mikhail Dubov", :cohort => "February", :gender => "male"},
+	{:name => "Karolis Noreika", :cohort => "February", :gender => "male"},
+	{:name => "Michael Sidon", :cohort => "February", :gender => "male"},
+	{:name => "Charles De Barros", :cohort => "February", :gender => "male"},
+	{:name => "Ruslan Vikhor", :cohort => "February", :gender => "male"},
+	{:name => "Toby Retallick", :cohort => "February", :gender => "male"},
+	{:name => "Mark Mekhaiel", :cohort => "February", :gender => "male"},
+	{:name => "Sarah Young", :cohort => "February", :gender => "male"},
+	{:name => "Hannah Wight", :cohort => "February", :gender => "male"},
+	{:name => "Khushkaran Singh", :cohort => "February", :gender => "male"},
+	{:name => "Rick brunstedt", :cohort => "February", :gender => "male"},
+	{:name => "Manjit Singh", :cohort => "February", :gender => "male"},
+	{:name => "Alex Gaudiosi", :cohort => "February", :gender => "male"},
+	{:name => "Ross Hepburn", :cohort => "February", :gender => "male"},
+	{:name => "Natascia Marchese", :cohort => "February", :gender => "male"},
+	{:name => "Tiffanie Chia", :cohort => "February", :gender => "male"},
+	{:name => "Matthew Thomas", :cohort => "February", :gender => "male"},
+	{:name => "Freddy McGroarty", :cohort => "February", :gender => "male"},
+	{:name => "Tyler Rollins", :cohort => "February", :gender => "male"},
+	{:name => "Richard Curteis", :cohort => "February", :gender => "male"},
+	{:name => "Anna Yanova", :cohort => "February", :gender => "male"},
+	{:name => "Mistery Man", :cohort => "February", :gender => "male"},
 ]
-
-febuarycohort = [
-	"Mario Gintili",
-	"Mikhail Dubov",
-	"Karolis Noreika",
-	"Michael Sidon",
-	"Charles De Barros",
-	"Ruslan Vikhor",
-	"Toby Retallick",
-	"Mark Mekhaiel",
-	"Sarah Young",
-	"Hannah Wight",
-	"Khushkaran Singh",
-	"Rick brunstedt",
-	"Manjit Singh",
-	"Alex Gaudiosi",
-	"Ross Hepburn",
-	"Natascia Marchese",
-	"Tiffanie Chia",
-	"Matthew Thomas",
-	"Freddy McGroarty",
-	"Tyler Rollins",
-	"Richard Curteis",
-	"Anna Yanova",
-	"Mistery Man",
-]
-
-
-students = [marchcohort, febuarycohort]
 
 def print_header
 	puts "Here's a list yo"
@@ -62,12 +56,14 @@ def print(names)
 	end
 end
 
-def print_footer
-puts "The Febuary cohort has #{febuarycohort.length} students.\n
-The March cohort has #{marchcohort.length} students.\n
-All in all, there are #{students.length} makers!"
+def print_footer(students)
+	students.each do |student|
+		puts"The February cohort has #{student[:cohort].include?("February").length} students."
+	end
+	puts"The March cohort has #{students.select{|element| element == "March"}.length} students."
+	puts"All in all, there are #{students.length} makers!"
 end
 
 print_header
 print(students)
-print_footer
+print_footer(students)
