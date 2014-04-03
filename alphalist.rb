8e7ad6ec96cfdbd9c4321ef34193af19c084fd0c
Joey Wolf <halfdark000@gmail.com>
2014-04-03 11:39:34 -0700

    code updated with march cohort, added some defs. BROKEN.

--- alphalist.rb
+++ alphalist.rb
@@ -1,5 +1,5 @@
 
-students = [
+marchcohort = [
 	"Steve Musgrave",
 	"Sroop Sunar",
 	"Colin Marshall",
@@ -22,9 +22,52 @@ students = [
 	"Daniel Le Dosquet"
 ]
 
-puts "Here's a list yo"
-puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-students.each do |student|
-	puts student
+febuarycohort = [
+	"Mario Gintili",
+	"Mikhail Dubov",
+	"Karolis Noreika",
+	"Michael Sidon",
+	"Charles De Barros",
+	"Ruslan Vikhor",
+	"Toby Retallick",
+	"Mark Mekhaiel",
+	"Sarah Young",
+	"Hannah Wight",
+	"Khushkaran Singh",
+	"Rick brunstedt",
+	"Manjit Singh",
+	"Alex Gaudiosi",
+	"Ross Hepburn",
+	"Natascia Marchese",
+	"Tiffanie Chia",
+	"Matthew Thomas",
+	"Freddy McGroarty",
+	"Tyler Rollins",
+	"Richard Curteis",
+	"Anna Yanova",
+	"Mistery Man",
+]
+
+
+students = [marchcohort, febuarycohort]
+
+def print_header
+	puts "Here's a list yo"
+	puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 end
-puts "Damn, G. There be like #{students.length}"
+
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer
+puts "The Febuary cohort has #{febuarycohort.length} students.\n
+The March cohort has #{marchcohort.length} students.\n
+All in all, there are #{students.length} makers!"
+end
+
+print_header
+print(students)
+print_footer
